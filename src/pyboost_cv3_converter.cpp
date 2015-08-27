e3fdeb19031f5dad682fc25596a4c7aa6c1d1978
Gregory Kramida <algomorph@gmail.com>
2015-08-26 20:18:07 -0400

    Added missing defines to top of cv3 converter source file

--- src/pyboost_cv3_converter.cpp
+++ src/pyboost_cv3_converter.cpp
@@ -5,7 +5,8 @@
  *      Author: Gregory Kramida
  *   Copyright: 2015 Gregory Kramida
  */
-
+#define NO_IMPORT_ARRAY
+#define PY_ARRAY_UNIQUE_SYMBOL pbcvt_ARRAY_API
 #include <pyboostcvconverter/pyboostcvconverter.hpp>
 #if CV_VERSION_MAJOR == 3
 namespace pbcvt{
