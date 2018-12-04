3d39e2933c9eb324cec6111b68c24fb64af3afca
DanManN <dnahimov@gmail.com>
2018-12-04 17:11:37 -0500

    change comment

--- src/pyboost_cv4_converter.cpp
+++ src/pyboost_cv4_converter.cpp
@@ -1,9 +1,6 @@
 /*
- * CV3BoostConverter.cpp
+ * CV4BoostConverter.cpp
  *
- *  Created on: May 21, 2015
- *      Author: Gregory Kramida
- *   Copyright: 2015 Gregory Kramida
  */
 #define NO_IMPORT_ARRAY
 #define PY_ARRAY_UNIQUE_SYMBOL pbcvt_ARRAY_API
